8bfc612f9eaaff06133258e93faaa591d38a247b
Amanda <Amanda>
2017-03-07 20:29:57 +0100

    rydding

---
+++ Oppgave3/tester.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("%c\n", "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98")
+
+}
--- unicode/unicode.go
+++ unicode/unicode.go
@@ -1,6 +1,12 @@
-package unicode
+package main
+
+import "fmt"
 
 // Kode for Oppgave 4a
 func Translate(expression string, language string) string {
 	return ""
+}
+
+func main() {
+	fmt.Println(Translate("jens", "hanne"))
 }
