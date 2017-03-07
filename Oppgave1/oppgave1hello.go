e47ea78b66939b37e6b3d623490877a1f0478b36
Amanda <Amanda>
2017-03-07 21:02:41 +0100

    lagg til anførseltegn

--- Oppgave1/oppgave1hello.go
+++ Oppgave1/oppgave1hello.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	str := "\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29"
+	str := "\x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"
 	fmt.Println()
 	fmt.Println(str)
 }
